14925bf1f014ddb5922e64f33d8897199edd0698
Shomegie <shomegie@gmail.com>
2022-08-29 21:51:45 +0100

    finalized concept layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
 <script>
 	import '../app.postcss';
+	// 		background-image: linear-gradient(to right bottom, #00a151, #00aa51, #00b350, #01bc4f, #0ac54d);	
+
 </script>
 
-<slot />
+<div class="h-screen max-h-screen overflow-y-hidden w-full main ">
+	
+	<slot />
+
+</div>
+
+
+
+<style>
+	/* .main{
+		background-image: linear-gradient(to right bottom, #008e4a, #009846, #00a241, #06ab3a, #13b530);
+	} */
+
+</style>
+
