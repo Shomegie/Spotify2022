14857e1f0656bb1c4de5ceec6b10a7c9fd166f28
Shomegie <shomegie@gmail.com>
2022-09-16 18:34:50 +0100

    artist page done. Next: individual track preview

--- src/routes/artist/[artist]/+page.js
+++ src/routes/artist/[artist]/+page.js
@@ -7,27 +7,24 @@ let spotify_parameters = {
         'Content-Type': "application/json"
     }
 }
-
-let get_tracks = async (url) =>{
-    let res = await fetch (url,spotify_parameters)
-    let tracks = await res.json()
-    return tracks 
-}
  
 export async function load({ params, fetch }) {
     try {
         let url = `https://api.spotify.com/v1/artists/${params.artist}/top-tracks?market=NG`
+        let artist_details_url = `https://api.spotify.com/v1/artists/${params.artist}`
+        const res_artist = await fetch(artist_details_url,spotify_parameters)
         const res = await fetch(url,spotify_parameters)
         const tracks = await res.json()    
+        const artist = await res_artist.json()
         const status_code = await res.status
         return {
           tracks,
-          status_code
+          status_code,
+          artist
         };
         
     } catch (err) {
         throw error(404, 'Not found');
 
     }
-
 }
--- src/routes/artist/[artist]/+page.svelte
+++ src/routes/artist/[artist]/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     export let data;
-    console.log(data.tracks.tracks)
+    console.log(data)
 
     let top_tracks = []
     if (data.status_code == 200)
@@ -12,26 +12,41 @@
 </script>
 
 <section>
-    <div on:click={goSomeWhereBack} class="text-white text-opacity-80 w-5/6 mx-auto cursor-pointer hover:text-green-100 transition duration-150">
-        <div class="flex">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" class="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-            </svg>              
-            Back
+    <div on:click={goSomeWhereBack} class="text-white w-5/6 mx-auto cursor-pointer pt-8 pl-3">
+        <div class="flex">         
+            <span class="mr-1 text-opacity-40 text-white hover:text-opacity-100 transition duration-100">&larr; Back</span>
         </div>
     </div>
 
 </section>
 
+<section>
+    <div class="w-5/6 lg:w-4/6 mx-auto py-7">
+        <div class=" flex items-end">
+            <!-- <div class="aspect-square h-32 bg-gray-500 mb-2 mr-2"></div> -->
+            {#if data.artist.images.length > 0}
+                <img class="aspect-square h-32 mb-2 mr-2" src={data.artist.images.slice(-1)[0].url} alt={data.artist.name}>
+            {:else}
+                <img class="aspect-square h-32 mb-2 mr-2" src="/images/logo.svg" alt={data.artist.name}>
+            {/if}
+            <div class="pb-2"><span class="italic">Artist: {data.artist.name}</span><br><span class="tracking-wide text-xs text-white text-opacity-60">Top tracks</span></div>
+        </div>
+    </div>
+</section>
+
 
 <section>
     {#if data.status_code == 200 }
-    <div class="grid md:grid-cols-2 gap-y-6 gap-x-6 w-5/6 lg:w-4/6 mx-auto lg:mt-96">
+    <div class="grid grid-cols-3 md:grid-cols-5 gap-4 md:gap-6 w-5/6 lg:w-4/6 text-xs mx-auto">
         {#each top_tracks as track, i }
-            <div class="p-2 border-white border-opacity-10 border rounded-xl text-white flex cursor-pointer hover:bg-white hover:text-black">
-                <div class="image"><img class="aspect-square h-24" src="/images/logo.svg" alt={i+1}></div>
-                <div class="track flex flex-col-reverse pb-4 pl-2">
-                    <p class="text-xs">Album name</p>
+            <div class=" border-white border-opacity-10 border rounded-xl text-white flex flex-col md:flex-col cursor-pointer hover:bg-white hover:text-black">
+                {#if track.album.images.length > 0}
+                <div class="w-fit mx-auto my-1 md:my-3 "><img class="aspect-square h-16 md:h-24" src={track.album.images[0].url} alt={track.name}></div>
+                {:else}
+                <div class="w-fit mx-auto md:mx-auto bg-yellow-300 my-1 md:my-3"><img class="aspect-square h-16 md:h-24" src="/images/logo.svg" alt={track.name}></div>
+                {/if}
+                <div class="track flex mx-auto text-center w-fit items-center md:mx-auto pl-2 mb-1">
+                    <!-- <p class="text-xs">Album name</p> -->
                     <p class="italic">{track.name}</p>
                 </div>
             </div>
